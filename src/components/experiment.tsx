65ea59d567a763274083650a79f608996a859132
Adrian Steffan <adrian.steffan@hotmail.de>
2024-11-25 02:43:05 +0100

    Add the ability to define custom components

--- src/components/experiment.tsx
+++ src/components/experiment.tsx
@@ -2,120 +2,133 @@
 
 import { now } from '../utils/common';
 import { useEffect, useState } from 'react';
+import { ComponentType } from 'react';
 
+// Default components
 import Upload from './upload';
 import Text from './text';
 import Quest from './quest';
 import MasterMindleWrapper from './mastermindlewrapper';
 import MicrophoneCheck from './microphonecheck';
 
-const componentMap = {
-    Text,
-    Quest,
-    Upload,
-    MicrophoneCheck,
-    MasterMindleWrapper,
-  };
+
+type ComponentsMap = {
+  [key: string]: ComponentType<any>;
+};
+
+// Default components map
+const defaultComponents: ComponentsMap = {
+  Text,
+  Quest,
+  Upload,
+  MicrophoneCheck,
+  MasterMindleWrapper,
+};
+
+interface ExperimentTrial {
+  name: string;
+  type: string;
+  props?: Record<string, any>;
+}
+
+interface TrialData {
+  index: number;
+  type: string;
+  name: string;
+  data: object | undefined;
+  start: number;
+  end: number;
+  duration: number;
+}
+
+interface ExperimentProps {
+  timeline: ExperimentTrial[];
+  components?: ComponentsMap;
+}
 
 // Function to transform experiment definition into components
 const transformExperiment = (
-    experimentDef: Array<{
-      name: string;
-      type: keyof typeof componentMap;
-      props?: Record<string, any>;
-    }>,
-    next: (data: any) => void,
-    data: any,
-    progress: number,
-  ) => {
-    return experimentDef.map((def, index) => {
-      const Component = componentMap[def.type];
-  
-      return (
-        <div className='px-4 w-screen'>
-          <div className='mt-4 sm:mt-12 max-w-2xl mx-auto flex-1 h-6 bg-gray-200 rounded-full overflow-hidden'>
-            <div
-              className={`h-full bg-gray-200 rounded-full duration-300 ${progress > 0 ? ' border-black border-2' : ''}`}
-              style={{
-                width: `${progress * 100}%`,
-                backgroundImage: `repeating-linear-gradient(
-                    -45deg,
-                    #E5E7EB,
-                    #E5E7EB 10px,
-                    #D1D5DB 10px,
-                    #D1D5DB 20px
-                  )`,
-                transition: 'width 300ms',
-              }}
-            />
-          </div>
-          {/* no type definitions for trial types at the moment, that will be a feature if we ever make this into a jspsych alternative */}
-          {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-          {/* @ts-ignore */}
-          <Component
-            {...{
-              key: index,
-              next,
-              data,
-              ...def.props,
+  experimentDef: ExperimentTrial[],
+  next: (data: any) => void,
+  data: any,
+  progress: number,
+  componentsMap: ComponentsMap
+) => {
+  return experimentDef.map((def, index) => {
+    // Look up component in provided components map
+    const Component = componentsMap[def.type];
+
+    if (!Component) {
+      throw new Error(`No component found for type: ${def.type}`);
+    }
+
+    return (
+      <div key={index} className='px-4 w-screen'>
+        <div className='mt-4 sm:mt-12 max-w-2xl mx-auto flex-1 h-6 bg-gray-200 rounded-full overflow-hidden'>
+          <div
+            className={`h-full bg-gray-200 rounded-full duration-300 ${
+              progress > 0 ? ' border-black border-2' : ''
+            }`}
+            style={{
+              width: `${progress * 100}%`,
+              backgroundImage: `repeating-linear-gradient(
+                -45deg,
+                #E5E7EB,
+                #E5E7EB 10px,
+                #D1D5DB 10px,
+                #D1D5DB 20px
+              )`,
+              transition: 'width 300ms',
             }}
           />
         </div>
-      );
-    });
-  };
-  
-  interface TrialData {
-    index: number;
-    type: string;
-    name: string;
-    data: object | undefined;
-    start: number;
-    end: number;
-    duration: number;
-  }
-  
-  export default function Experiment({timeline}: {timeline: any[]}) {
-    const [trialCounter, setTrialCounter] = useState(0);
-    const [data, setData] = useState<TrialData[]>([]);
-    const [trialStartTime, setTrialStartTime] = useState(now());
-  
-    useEffect(() => {
-      window.scrollTo(0, 0)
-    }, [trialCounter])
-  
-    function next(newData?: object): void {
-      const currentTime = now();
-  
-      // Get the current trial information from the experiment
-      // no type definitions for trial types at the moment, that will be a feature if we ever make this into a jspsych alternative
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      const currentTrial: ExperimentTrial = timeline[trialCounter];
-  
-      if (currentTrial && data) {
-        const trialData: TrialData = {
-          index: trialCounter,
-          type: currentTrial.type,
-          name: currentTrial.name,
-          data: newData,
-          start: trialStartTime,
-          end: currentTime,
-          duration: currentTime - trialStartTime,
-        };
-        //console.log([...data, trialData]);
-        setData([...data, trialData]);
-      }
-  
-      // Set the start time for the next trial
-      setTrialStartTime(currentTime);
-      setTrialCounter(trialCounter + 1);
+        <Component
+          next={next}
+          data={data}
+          {...def.props}
+        />
+      </div>
+    );
+  });
+};
+
+export default function Experiment({ timeline, components = {} }: ExperimentProps) {
+  const [trialCounter, setTrialCounter] = useState(0);
+  const [data, setData] = useState<TrialData[]>([]);
+  const [trialStartTime, setTrialStartTime] = useState(now());
+
+  const componentsMap = { ...defaultComponents, ...components };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [trialCounter]);
+
+  function next(newData?: object): void {
+    const currentTime = now();
+    const currentTrial = timeline[trialCounter];
+
+    if (currentTrial && data) {
+      const trialData: TrialData = {
+        index: trialCounter,
+        type: currentTrial.type,
+        name: currentTrial.name,
+        data: newData,
+        start: trialStartTime,
+        end: currentTime,
+        duration: currentTime - trialStartTime,
+      };
+      setData([...data, trialData]);
     }
-  
-    // no type definitions for trial types at the moment, that will be a feature if we ever make this into a jspsych alternative
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-ignore
-    return transformExperiment(timeline, next, data, trialCounter / (timeline.length - 1))[
-      trialCounter
-    ];
+
+    setTrialStartTime(currentTime);
+    setTrialCounter(trialCounter + 1);
   }
+
+  return transformExperiment(
+    timeline,
+    next,
+    data,
+    trialCounter / (timeline.length - 1),
+    componentsMap
+  )[trialCounter];
+}

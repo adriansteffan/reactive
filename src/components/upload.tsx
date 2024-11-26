d5f7682ae3e953c90651db4f8b546eaea00ba034
Adrian Steffan <adrian.steffan@hotmail.de>
2024-11-26 14:46:54 +0100

    upload related cleanup

--- src/components/upload.tsx
+++ src/components/upload.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { v4 as uuidv4 } from 'uuid';
 import { post } from '../utils/request';
-import { FileUpload, StudyEvent } from '../utils/convert';
+import { FileUpload, StudyEvent } from '../utils/common';
 
 interface UploadPayload {
   sessionId: string;

ebcff59e2ae5ded5bb1634eeda94321544f3cce0
Adrian Steffan <adrian.steffan@hotmail.de>
2024-11-30 21:51:57 +0100

    deployment fixes

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true, // needed due to tailwind injection order when importing reactive-psych
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [],
   presets: [require("reactive-psych/tailwind.config.js")],

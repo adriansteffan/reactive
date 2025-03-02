6a42149f49d1a9218d50d3af6aa96b89b8e3b6c9
Adrian Steffan <adrian.steffan@hotmail.de>
2025-03-02 19:03:12 +0100

    remove template in prep for tailwind migration

--- template/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  important: true, // needed due to tailwind injection order when importing reactive
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  plugins: [require('@tailwindcss/typography')],
-  presets: [require("@adriansteffan/reactive/tailwind.config.js")],
-};

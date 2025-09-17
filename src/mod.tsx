23969616b11f31c7fd7db6a0e73faffc6bbb119c
Adrian Steffan <adrian.steffan@hotmail.de>
2025-09-17 10:55:43 +0200

    Fix toast for consumer

--- src/mod.tsx
+++ src/mod.tsx
@@ -5,3 +5,5 @@ export type { BaseComponentProps, ExperimentConfig };
 export * from './utils/array';
 export * from './utils/common';
 export * from './components';
+
+export * from 'react-toastify';
